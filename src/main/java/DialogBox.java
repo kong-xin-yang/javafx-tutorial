ff31a1e6350591831447ebbecebb4ee711a00765
kong-xin-yang <0kingkng0@gmail.com>
2026-09-05 17:15:26 +0800

    Add basic chatbot elements + formatting

---
+++ src/main/java/DialogBox.java
@@ -0,0 +1,24 @@
+import javafx.geometry.Pos;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
+
+public class DialogBox extends HBox {
+    private Label text;
+    private ImageView displayPicture;
+
+    // Constructor
+    public DialogBox(String input, Image image) {
+        this.text = new Label(input);
+        this.displayPicture = new ImageView(image);
+
+        // Formatting
+        text.setWrapText(true);
+        displayPicture.setFitWidth(100.0);
+        displayPicture.setFitHeight(100.0);
+        this.setAlignment(Pos.TOP_RIGHT);
+
+        this.getChildren().addAll(text, displayPicture);
+    }
+}
